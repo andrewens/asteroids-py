e068f3f7cd3abcf6cb31e25b64fa3dc028a719cb
andrewens <andrewens4@gmail.com>
2025-01-07 17:58:46 -0700

    Draw player as a triangle / rotate with A & D keys

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     print("Starting asteroids!")
@@ -10,16 +12,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(PLAYER_SPAWN_X, PLAYER_SPAWN_Y)
+
     # main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-        screen.fill(BACKGROUND_COLOR)
-        pygame.display.flip()
 
-        dt = clock.tick() * 1E-3
+        dt = clock.tick() * 1e-3
+        player.update(dt)
+
+        screen.fill(BACKGROUND_COLOR)
+        player.draw(screen)
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
